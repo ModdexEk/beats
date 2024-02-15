f02176415c3d20cf5e7f33fb87ad8664917ce359
Moddex <152146781+ModdexEk@users.noreply.github.com>
2024-02-15 17:43:13 +0500

    2

--- js/switcher.js
+++ js/switcher.js
@@ -1,8 +1,17 @@
+const findBlockByAlias = alias => {
+    return $(".reviews__item").filter((ndx, item) => {
+       return $(item).attr("data-linked-with") == alias;
+    })
+}
+
 $(".interactive-avatar__link").click (e => {
     e.preventDefault();
 
     const $this = $(e.currentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlockByAlias(target);
     const curItem = $this.closest(".interactive-avatar");
 
+    itemToShow.addClass("activ").siblings().removeClass("activ");
     curItem.addClass("interactive-avatar--activ").siblings().removeClass("interactive-avatar--activ");
 })
