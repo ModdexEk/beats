47b01ca33480a5a0062072f2bfe42a4e8de75d3a
Moddex <152146781+ModdexEk@users.noreply.github.com>
2024-02-22 15:43:46 +0500

    2

---
+++ js/map.js
@@ -0,0 +1,44 @@
+/* let myMap;
+
+const init = () => {
+    myMap = new ymap.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    })
+}
+
+ymaps.ready(init);
+
+ */
+
+
+
+
+initMap();
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [37.588144, 55.733842],
+
+                // Уровень масштабирования
+                zoom: 10
+            }
+        }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
